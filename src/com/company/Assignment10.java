af75706939865888d6e1eb5079f58839f103d38e
Sanvi Rathore <rathoresanvi3@gmail.com>
2024-09-30 13:41:18 +0530

    Initial Commit

--- src/com/company/Assignment10.java
+++ src/com/company/Assignment10.java
@@ -13,8 +13,19 @@ public class Assignment10 {
         key(arr,i+1,key);
     }
 
+    static String digits[]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    public static void year(int num){
+        if(num==0){
+            return;
+        }
+        int ld=num%10;
+        year(num/10);
+        System.out.print(digits[ld]+" ");
+    }
+
     public static void main(String[] args) {
-        int arr[]={3, 2, 4, 5, 6, 2, 7, 2, 2};
-        key(arr,0,2);
+//        int arr[]={3, 2, 4, 5, 6, 2, 7, 2, 2};
+//        key(arr,0,2);
+        year(2019);
     }
 }
