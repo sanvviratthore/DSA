f681b07bdbc3768a31e255daef9dda261ee31330
Sanvi Rathore <rathoresanvi3@gmail.com>
2024-10-04 20:46:56 +0530

    Divide & Conquer

--- src/com/company/D_C.java
+++ src/com/company/D_C.java
@@ -9,6 +9,52 @@ public class D_C {
         System.out.println();
     }
 
+    public static void mergeSort(int arr[], int si, int ei){
+
+        if(si>=ei){
+            return;
+        }
+        // kaam
+        int mid= si+(ei-si)/2;
+
+        mergeSort(arr,si,mid); //left part
+        mergeSort(arr,mid+1,ei); //right part
+        merge(arr,si,mid,ei);
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei){
+        int temp[]=new int[ei-si+1];
+        int i=si; // iterator for left part
+        int j=mid+1; //iterator for right part
+        int k=0; // iterator for temp array
+
+        while (i<=mid && j<=ei){
+            if(arr[i] < arr[j]){
+                temp[k]=arr[i];
+                i++;
+            } else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // left part
+        while (i<=mid){
+            temp[k++]=arr[i++];
+        }
+
+        //right part
+        while (j<=ei){
+            temp[k++]=arr[j++];
+        }
+
+        // copy temp to original array
+        for(k=0, i=si; k< temp.length; k++, i++){
+            arr[i]=temp[k];
+        }
+    }
+
     public static void quickSort(int arr[], int si, int ei){
 
         if(si>=ei){
@@ -78,11 +124,13 @@ public class D_C {
 
 
     public static void main(String[] args) {
-        int arr[]={4,5,6,7,0,1,2};
+        int arr[]={4,5,6,7,0,1,2,-1};
         int tar=0; //output -->4
 //        quickSort(arr,0, arr.length-1);
 //        printArr(arr);
-        int target=search(arr,tar,0, arr.length-1);
-        System.out.println(target);
+//        int target=search(arr,tar,0, arr.length-1);
+//        System.out.println(target);
+        mergeSort(arr,0, arr.length-1);
+        printArr(arr);
     }
 }
