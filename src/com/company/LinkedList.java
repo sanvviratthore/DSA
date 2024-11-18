8cd1b6c904928ec23b56a7c91bec06f1bb83215f
Sanvi Rathore <rathoresanvi3@gmail.com>
2024-11-18 19:02:19 +0530

    Updated Linked Lists

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -110,25 +110,211 @@ public class LinkedList {
         size--;
         return val;
     }
+    public int itrSearch(int key){ //O(n)
+        Node temp=head;
+        int i=0;
+
+        while(temp!=null){
+            if(temp.data == key){ // key found
+                return i;
+            }
+            temp=temp.next;
+            i++;
+        }
+
+        //key not found
+        return -1;
+    }
+
+    public int helper(Node head, int key){
+        if(head==null){
+            return -1;
+        }
+
+        if(head.data==key){
+            return 0;
+        }
+        int idx=helper(head.next, key);
+        if(idx==-1){
+            return -1;
+        }
+        return idx+1;
+    }
+    public int recSearch(int key){ //O(n)
+        return helper(head,key);
+    }
+
+    public void reverse(){ //O(n)
+        Node prev=null;
+        Node curr=tail=head;
+        Node next;
+
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+    }
+
+    public void deleteNthfromEnd(int n){
+        // calc size
+        int sz=0;
+        Node temp=head;
+        while (temp!=null){
+            temp=temp.next;
+            sz++;
+        }
+
+        if(n == sz){ // corner case
+            head=head.next;
+            return;
+        }
+
+        //sz-n ie just before the node we need to delete
+        int i=1;
+        int iToFind=sz-n;
+        Node prev=head;
+        while (i<iToFind){
+            prev=prev.next;
+            i++;
+        }
+
+        prev.next=prev.next.next;
+        return;
+    }
+
+    //slow-fast approach
+    public Node findMid(Node head){ //helper fn
+        Node fast=head;
+        Node slow=head;
+
+        while (fast!=null && fast.next!=null){
+            slow=slow.next; //+1
+            fast=fast.next; //+2
+        }
+        return slow; // slow is my midNode
+    }
+    public boolean checkPalindrome(){
+        if(head==null || head.next==null){
+            return true;
+        }
+        //step-1 find mid
+        Node midNode= findMid(head);
+
+        //step-2 rev 2nd half
+        Node prev=null;
+        Node curr=midNode;
+        Node next;
+        while (curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+
+        Node right=prev; // right half head
+        Node left=head;
+
+        //step-3 check left half and right half
+        while (right!=null){
+            if(left.data!= right.data){
+                return false;
+            }
+            left=left.next;
+            right=right.next;
+        }
+        return true;
+    }
+
+    public static boolean isCycle(){
+        Node slow=head;
+        Node fast=head;
+
+        while (fast!=null && fast.next!=null){
+            slow=slow.next; //+1
+            fast=fast.next.next; //+2
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false; //cycle doesnot exist
+    }
+
+    public static void removeCycle(){
+        //detect cycle
+        Node slow= head;
+        Node fast= head;
+        boolean cycle = false;
+        while (fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                cycle=true;
+                break;
+            }
+        }
+        if(cycle==false){
+            return;
+        }
+        //find meeting point
+        slow=head;
+        Node prev=null;
+        while (slow!=fast){
+            prev=fast;
+            slow=slow.next;
+            fast=fast.next;
+        }
+        //remove cycle -> last.next=null
+        prev.next=null;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         //ll.print();
-        ll.addFirst(2);
-        //ll.print();
-        ll.addFirst(1);
-        //ll.print();
-        ll.addLast(4);
-        //ll.print();
-        ll.addLast(5);
-
-        ll.add(2,3);
-        ll.print();
+//        ll.addFirst(2);
+//        //ll.print();
+//        ll.addFirst(1);
+//        //ll.print();
+//        ll.addLast(4);
+//        //ll.print();
+//        ll.addLast(5);
+//
+//        ll.add(2,3);
+//        ll.print();
         //System.out.println(ll.size);
-        ll.removeFirst();
-        ll.print();
+//        ll.removeFirst();
+//        ll.print();
 
-        ll.removeLast();
-        ll.print();
-        System.out.println(ll.size);
+//        ll.removeLast();
+//        ll.print();
+        //System.out.println(ll.size);
+//        System.out.println(ll.itrSearch(3));
+//        System.out.println(ll.itrSearch(10));
+//        System.out.println(ll.recSearch(3));
+//        System.out.println(ll.recSearch(10));
+//        ll.reverse();
+//        ll.print();
+//         ll.deleteNthfromEnd(3);
+//         ll.print();
+
+//        ll.addLast(1);
+//        ll.addLast(2);
+//        ll.addLast(2);
+//        //ll.addLast(1);
+//        ll.print();
+//        System.out.println(ll.checkPalindrome());
+
+        head= new Node(1);
+        Node temp=new Node(2);
+        head.next=new Node(2);
+        head.next=temp;
+        head.next.next= new Node(3);
+        head.next.next.next=temp;
+        //1->2->3->2
+        System.out.println(isCycle());
+        removeCycle();
+        System.out.println(isCycle());
     }
 }
