64c46f10e10919b678e545ecdf19b612f90d6ab8
Sanvi Rathore <rathoresanvi3@gmail.com>
2024-11-25 18:39:51 +0530

    Updated Stacks

--- src/com/company/Stacks.java
+++ src/com/company/Stacks.java
@@ -168,6 +168,76 @@ public class Stacks {
             return false;
         }
     }
+
+    public static boolean isDuplicate(String str){ //O(n)
+        Stack<Character> s = new Stack<>();
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+
+            //closing
+            if(ch == ')'){
+                int count = 0;
+                while (s.peek() != '('){
+                    s.pop();
+                    count++;
+                }
+                if(count < 1){
+                    return true; //duplicate
+                } else {
+                    s.pop(); //opening pair
+                }
+            } else{
+                //opening
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+
+    public static void maxArea(int[] arr){ //-> O(n)
+        int maxArea = 0;
+        int[] nsr = new int[arr.length];
+        int[] nsl = new int[arr.length];
+
+        //next smaller right -> O(n)
+        Stack<Integer> s = new Stack<>();
+        for(int i= arr.length-1; i>=0; i--){
+            while (!s.isEmpty() && arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsr[i] = arr.length;
+            } else{
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        //next smaller left O(n)
+        s = new Stack<>();
+        for(int i= 0; i< arr.length; i++){
+            while (!s.isEmpty() && arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsl[i] = -1;
+            } else{
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        //current area : width = j-i-1 = nsr[i]-nsl[i]-1 -> O(n)
+        for(int i=0; i< arr.length; i++){
+            int height = arr[i];
+            int width = nsr[i]-nsl[i]-1;
+            int currArea = height*width;
+            maxArea = Math.max(currArea, maxArea);
+        }
+
+        System.out.println("Max area in histogram is: "+maxArea);
+    }
     public static void main(String[] args) {
         //Stack<Integer> s = new Stack<>();
 //        s.push(1);
@@ -223,7 +293,14 @@ public class Stacks {
 //        }
 //        System.out.println();
 
-        String str = "({})[]";
-        System.out.println(isValid(str));
+//        String str = "({})[]";
+//        System.out.println(isValid(str));
+
+//        String str = "((a+b))"; //true
+//        String str2 = "(a-b)"; //false;
+//        System.out.println(isDuplicate(str2));
+
+        int[] arr = {2,1,5,6,2,3}; //heights in histogram
+        maxArea(arr);
     }
 }
